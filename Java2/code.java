d8db9712191a8727a9bdfc11717503bd00aa7825
Akash <Akash.patel.ap88020@gmail.com>
2025-05-04 14:40:56 +0530

    learnt in java about data-types , variables or string and there strings method

---
+++ Java2/code.java
@@ -0,0 +1,32 @@
+public class code {
+    public static void main(String[] args){
+        // System.out.println("hllow world");
+        // System.out.println("hllow world");
+
+        // Variable
+
+        // String friends = "Ashish";
+        // int age = 20;
+        // System.out.println(age);
+        
+        // Data-Type
+        
+        
+        String name = "John";
+        String name1 = "Doe";
+        byte age = 30;
+        int phone = 1234567890;
+        Long phone2 = 12345678903L;
+        float pi = 3.47f;
+        char letter = '@';
+        boolean isAdult = true;
+
+
+        String concatinate = name + " and " + name1;
+        // System.out.println(concatinate.charAt(0));
+        // System.out.println(concatinate.length());
+        // System.out.println(concatinate.replace(name1, "Sinha"));
+        System.out.println(concatinate.substring(0,4));
+
+    }
+}

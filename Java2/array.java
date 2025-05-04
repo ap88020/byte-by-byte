840ea5fcf16ce94c650bcaf3a174922e14777749
Akash <Akash.patel.ap88020@gmail.com>
2025-05-04 15:00:00 +0530

    learnt about array in java

---
+++ Java2/array.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+import javax.sound.midi.SysexMessage;
+
+public class array {
+    public static void main(String[] args){
+        // int physics = 199;
+        // int chemestry = 99;
+        // int math = 100;
+
+        // int marks[] = new int[3];
+
+        // marks[0] = physics;
+        // marks[1] = chemestry;
+        // marks[2] = math;
+
+        // System.out.println(marks[1]);
+        // System.out.println(marks.length);
+        
+        // System.out.println(marks[0]);
+        // Arrays.sort(marks);
+        // System.out.println(marks[0]);
+
+        // int[] marks = {1,2,3,4,5};
+        // System.out.println(marks.length);
+        // System.out.println(marks[3]); 
+
+        int[][] marks = { {77,33,97}, {22,73,67} };
+        System.out.println(marks[1][2]);
+    }
+}

571249927c21cc6446adbfd4dae30616bc886cde
Akash <Akash.patel.ap88020@gmail.com>
2025-08-16 15:54:42 +0530

    lernt data-types or variables in python

---
+++ PYTHON/Data-Types-Variables.py
@@ -0,0 +1,66 @@
+# print("hllo world","this is another world");
+
+name = "akash";
+# char = 'a';
+age = 20;
+price = 45.99;
+old = True; # not->true , yes->True; 
+a=None;
+
+# Arithmetic Operator
+b = 4;
+c = 3;
+
+# print(b+c);
+# print(b-c);
+# print(b/c);
+# print(b*c);
+# print(b%c);
+# print(b**c);
+
+# Relational Operator
+a = 20;
+b = 30;
+
+# print(a == b); #False
+# print(a != b); #True
+# print(a < b); #True
+# print(a > b); #False
+# print(a <= b); #ture
+
+# Logical Operator
+
+# print(not False)
+# print(not True)
+
+# Type Conversion
+
+a = int("30");
+b = 50.48;
+c = float("2.44");
+
+# print(a+b+c);
+
+#Input Statement
+
+numA = int(input("enter number a : "));
+numB = int(input("enter number b : "));
+
+print(numA >= numB);
+# prin(numA >= numB);
+# print(numA + numB / 2)
+# print("type of numA is : ", type(numA) , "type of numB is :" , type(numB) , "sum of numA and numB is :"  , numA + numB);
+
+# name = input("Enter your name : ");
+# print("you are most welcome ",name);
+
+# print(type(name));
+# print(type(char));
+# print(type(age));
+# print(type(price));
+# print(type(old));
+# print(type(a));
+
+# print("my name is : ",name);
+# print("my age is : ",age);
+# print(price);

0cb9066a541c2cd0ff3ed0e6529e82edf1a5ae3e
Akash <Akash.patel.ap88020@gmail.com>
2025-08-17 07:59:05 +0530

    learnt string and conditional statement

---
+++ PYTHON/string-Or-conditionals-statements.py
@@ -0,0 +1,72 @@
+num = int(input("enter you marks : "));
+
+
+if(num % 7 == 0):
+    print("multiple of 7")
+else: 
+    print("not multiple of 7")
+
+
+# if(marks % 2 == 0):
+#     print("even")
+# else: 
+#     print("odd");
+# if(marks >= 90):
+#     print("Grade : A")
+# elif(marks >= 70 and marks < 90):
+#     print("Grade : B")
+# elif(marks >= 60 and marks < 80):
+#     print("Grade : C")
+# else:
+#     print("Failed")
+
+# age = 72;
+
+# if(age >= 18):
+#     print("can vote")
+#     print("can drive")
+# else:
+#     print("cannot vote")
+#     print("cannot drive")
+
+# color = "pink";
+
+# if(color == "red"):
+#     print("stop")
+# elif(color == "yellow"):
+#     print("look")
+# elif(color == "green"):
+#     print("go")
+# else:
+#     print("go to hell")
+
+# str = str(input("enter you name : "));
+# print(len(str));
+# print(str.find("$"));
+
+# str = "i am learnin pyhton";
+# print(str.endswith("ono"));
+# print(str.capitalize());
+# print(str.replace("a","YY"));
+# print(str.find("pyhton"));
+# print(str.count("am"));
+
+# str1 = "hllo";
+# str2 = "world";
+# final_str = str1 +" "+ str2;
+
+# final_str[8] = "@";
+# print(len(final_str));
+# print(final_str);
+# # print(final_str[8])
+# print(final_str[2:len(final_str)]);
+# print(final_str[2:]);
+
+
+# str = "this is string \nthis is pythong"
+# print(str);
+
+# str1 = "this is stirng"
+# str2 = 'this is stirng'
+# str3 = """this is stirng"""
+# print("hllo world");
